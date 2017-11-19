32aa7e31fa26f14d99cb16d785b5b3b37591d0af
Pavel Smelov <pavsmel@mail.ru>
2017-11-19 22:17:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,22 +3,26 @@ $( document ).ready (function () {
     $("#ul").append('<li id = "li"></li>');
     $("#li").append('<span>Сделать задание #3 по web-программированию</span>' );
     $("#li").append('<button class="button" >Удалить</button>');
-    $('#root').append('<input id="add_task_input">')
+    $('#root').append('<input id="add_task_input">');
     $('#root').append('<button id = "add_task">Добавить</button>');
     $("#add_task").click(function(){
         var text = document.getElementsByTagName("input")[0];
         var val = text.value;
-        $("#ul").append('<li id = "new"></li>');
-        $('#new').append(val);
-        $("#new").append('<button class="button" >Удалить</button>');
+        var newLi = document.createElement('li');
+        var newSpan = document.createElement('span');
+        $(newSpan).append(val);
+        $(newLi).append(newSpan);
+        $(newLi).append('<button class="button" >Удалить</button>');
+        $('#ul').append(newLi);
         $(".button").click(function(){
-            $("li").remove();
+            $(newLi).remove();
         });
     });
     $(".button").click(function(){
-        $("li").remove();
+        $("#li").remove();
     });
 });
+
 
 
 
